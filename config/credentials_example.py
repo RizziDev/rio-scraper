9e63b6ee5812313f24fb0cc34b4cc09fd1dd3609
RizziDev <102371142+RizziDev@users.noreply.github.com>
2025-04-14 16:12:09 -0300

    Create credentials_example.py

---
+++ config/credentials_example.py
@@ -0,0 +1,5 @@
+# config/credentials_example.py
+
+# Exemplo de credenciais. Substitua pelos seus dados e use variáveis de ambiente para segurança.
+LOGIN_EMAIL = "seu.email@exemplo.com"
+LOGIN_SENHA = "sua_senha_super_secreta"
